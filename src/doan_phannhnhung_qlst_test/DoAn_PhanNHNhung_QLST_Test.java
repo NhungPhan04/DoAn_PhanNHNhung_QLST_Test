2e66ea5e29a6c6be07d55c36909192cd9a85aa68
dell <dell@169.254.100.235>
2020-04-13 08:45:01 +0700

    this is a demo project

---
+++ src/doan_phannhnhung_qlst_test/DoAn_PhanNHNhung_QLST_Test.java
@@ -0,0 +1,13 @@
+
+package doan_phannhnhung_qlst_test;
+
+public class DoAn_PhanNHNhung_QLST_Test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
